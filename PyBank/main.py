326805a789c2d09e1ce7e06f51f9b697d0d359d3
Mike Hills <hills072@umn.edu>
2021-01-22 16:19:33 -0600

    Total months and net total amount of Profit/Losses over entire period

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,26 @@
-print("Hello from PyBank")
+import csv
+
+with open('budget_data.csv', 'r') as budget_data:
+    csv_reader = csv.reader(budget_data)
+
+    next(csv_reader)
+
+    total_months = 0
+
+    for row in csv_reader:
+        total_months += 1
+    
+    print (total_months)
+
+    # monthly_total = 0
+
+    # for row in csv_reader:
+    #     monthly_total += int(row[1])
+        
+    # print(monthly_total)
+
+
+    # for line in csv_reader:
+    #     print(line)
+
+
