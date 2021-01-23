a15650335419dbb7ca1ddb8c79180c0f3596fc29
Mike Hills <hills072@umn.edu>
2021-01-22 20:17:22 -0600

    reorganized code and added changes in profit/losses code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+import os
 import csv
 
 with open('budget_data.csv', 'r') as budget_data:
@@ -5,22 +6,33 @@ with open('budget_data.csv', 'r') as budget_data:
 
     next(csv_reader)
 
+    #initialize counter variables
     total_months = 0
+    monthly_total = 0
 
+    #create empty lists to store data
+    monthly_change = []
+    profitloss_list = []
+    monthly_change = []
+
+    #calculate total months and net profit/losses over entire period
     for row in csv_reader:
         total_months += 1
+        monthly_total += int(row[1])
+        profitloss_list.append(row[1])
     
-    print (total_months)
-
-    # monthly_total = 0
-
-    # for row in csv_reader:
-    #     monthly_total += int(row[1])
+    print(total_months)
+    print(monthly_total)
+    
+    #use range(len() to "iterate over numbers used as indices" in order to begin at index 1. 
+    #source: https://www.kite.com/python/answers/how-to-use-range(len())-in-python#:~:text=Use%20range()%20and%20len,access%20the%20items%20in%20obj%20.
+    for i in range(1, len(profitloss_list)):
         
-    # print(monthly_total)
+        #calculate monthly change
+        monthly_change.append(int(profitloss_list[i]) - int(profitloss_list[i-1]))
 
+        #calculate total average change
+        average_change = sum(monthly_change) / len(monthly_change)
+    
 
-    # for line in csv_reader:
-    #     print(line)
-
-
+    print(average_change)
