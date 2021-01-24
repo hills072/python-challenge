0ad7addb60a0206d399ec1acd316244c6b48cc1e
Mike Hills <hills072@umn.edu>
2021-01-23 21:48:57 -0600

    All values calculated.  Need to fix rounding formatting in percent votes won

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,5 @@
 import csv
 
-
-
 with open("Resources/election_data.csv", "r") as election_data:
     csv_reader = csv.reader(election_data)
 
@@ -13,42 +11,50 @@ with open("Resources/election_data.csv", "r") as election_data:
     Li = 0
     OTooley = 0
 
-    candidates = []
+    candidates_column = []
 
     for row in csv_reader:
         total_votes += 1
-        candidates.append([row[2]])
+        candidates_column.append([row[2]])
 
         
-    Khan = sum("Khan" in i for i in candidates)
-    Correy = sum("Correy" in i for i in candidates)
-    Li = sum("Li" in i for i in candidates)
-    OTooley = sum("O'Tooley" in i for i in candidates)
+    Khan_tot = sum("Khan" in i for i in candidates_column)
+    Correy_tot = sum("Correy" in i for i in candidates_column)
+    Li_tot = sum("Li" in i for i in candidates_column)
+    OTooley_tot = sum("O'Tooley" in i for i in candidates_column)
 
-    KhanPercent = (Khan / (Khan + Correy + Li + OTooley)) * 100
-    CorreyPercent = (Correy / (Khan + Correy + Li + OTooley)) * 100
-    LiPercent =  (Li / (Khan + Correy + Li + OTooley)) * 100
-    OTooleyPercent = (OTooley / (Khan + Correy + Li + OTooley)) * 100
+    Pop_vote_summary = {"Khan": Khan_tot, "Correy": Correy_tot, "Li": Li_tot, "O'Tooley": OTooley_tot}
+
+    winner = max(Pop_vote_summary, key=Pop_vote_summary.get)
+
+    KhanPercent = (Khan_tot / (Khan_tot + Correy_tot + Li_tot + OTooley_tot)) * 100
+    CorreyPercent = (Correy_tot / (Khan_tot + Correy + Li_tot + OTooley_tot)) * 100
+    LiPercent =  (Li_tot / (Khan_tot + Correy_tot + Li_tot + OTooley_tot)) * 100
+    OTooleyPercent = (OTooley_tot / (Khan_tot + Correy_tot + Li_tot + OTooley_tot)) * 100
+
     
-    print(f'{round(KhanPercent, 5)}%')
-
-    print(f'{round(CorreyPercent, 5)}%')
-
-    print(f'{round(LiPercent, 5)}%')
-
-    print(f'{round(OTooleyPercent, 5)}%')
-
+    
+    # print(winner)
+    # print(Pop_vote_summary)
+    # print(f'{round(KhanPercent, 5)}%')
+    # print(f'{round(CorreyPercent, 5)}%')
+    # print(f'{round(LiPercent, 5)}%')
+    # print(f'{round(OTooleyPercent, 5)}%')
     # print(Khan)
     # print(Correy)
     # print(Li)
     # print(OTooley)
-
-            
-        
-    
-
     # print(total_votes)
     
-    
-        
+    print("Election Rresults")
+    print("--------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("--------------------------")
+    print(f"Khan: {KhanPercent} ({Khan_tot})")
+    print(f"Correy: {CorreyPercent} ({Correy_tot})")
+    print(f"Li: {LiPercent} ({Li_tot})")
+    print(f"O'Tooley: {OTooleyPercent} ({OTooley_tot})")
+    print("--------------------------") 
+    print(f"Winner: {winner}")
+    print("--------------------------")      
         
